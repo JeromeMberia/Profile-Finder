ea604bf159ed774102dbe8d52320101b829bc1cf
JeromeMberia <jeromemberia@gmail.com>
2020-04-19 13:38:28 +0300

    Repository error

--- src/app/github-service/github.service.ts
+++ src/app/github-service/github.service.ts
@@ -1,3 +1,4 @@
+import { Repository } from './../repository';
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
@@ -10,51 +11,47 @@ import { environment } from 'src/environments/environment';
 })
 export class GithubService {
 
+  constructor(private http: HttpClient) {
+    this.user = new User('', '', '');
+   }
+
   userUrl = 'https://api.github.com/search/users?q=';
   user: User;
   users: User[] = [];
   data: any;
   username: string;
 
-  constructor(private http: HttpClient) {
-    this.user = new User('', '', '');
-   }
-
-   getUser(username: string) {
+  getUser(username: string) {
      this.username = username;
-
    }
 
+  userRequest() {
+    this.users.length = 0;
 
-
-   userRequest() {
-     this.users.length = 0;
-
-     const promise = new Promise((resolve, reject) => {
-       const userRequestUrl = this.userUrl + this.username;
-       console.log(userRequestUrl);
-       this.http.get(userRequestUrl).toPromise().then(response => {
-         this.data = response;
-         for (let i = 0; i < 1; i++) {
-         this.user.username = this.data.items[i].login;
-         this.user.image = this.data.items[i].avatar_url;
-         this.user.url = this.data.items[i].html_url;
-         this.users.push(
-           new User(
-             this.user.username,
-             this.user.url,
-             this.user.image
-            ));
-        }
-         resolve();
-       },
-       error => {
-         this.user.error = 'Please Enter a Valid Username';
-         reject(error);
-       });
-     });
-     return promise;
-   }
-
+    const promise = new Promise((resolve, reject) => {
+      const userRequestUrl = this.userUrl + this.username;
+      console.log(userRequestUrl);
+      this.http.get(userRequestUrl).toPromise().then(response => {
+        this.data = response;
+        for (let i = 0; i < 1; i++) {
+        this.user.username = this.data.items[i].login;
+        this.user.image = this.data.items[i].avatar_url;
+        this.user.url = this.data.items[i].html_url;
+        this.users.push(
+          new User(
+            this.user.username,
+            this.user.url,
+            this.user.image
+          ));
+      }
+        resolve();
+      },
+      error => {
+        this.user.error = 'Please Enter a Valid Username';
+        reject(error);
+      });
+    });
+    return promise;
+  }
 }
 
